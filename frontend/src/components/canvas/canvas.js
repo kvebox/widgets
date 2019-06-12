7916eb8712925eb6095135cb267377612d0924ff
victoria <vjohkv@gmail.com>
2019-06-12 12:08:36 -0700

    Complete drawing functionality on canvas for rudimentary drawing app

--- frontend/src/components/canvas/canvas.js
+++ frontend/src/components/canvas/canvas.js
@@ -1,106 +1,71 @@
 import React from 'react';
-import * as THREE from 'three';
-
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-
-// var geometry = new THREE.BoxGeometry(1, 1, 1);
-// var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// var cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-
-// camera.position.z = 5;
-
-// function animate() {
-//     requestAnimationFrame(animate);
-//     cube.rotation.x += 0.01;
-//     cube.rotation.y += 0.01;
-//     renderer.render(scene, camera);
-// }
-// animate();
-
-// class Canvas extends React.Component {
-
-    
-//     render() {
-//         return (
-//             <div>
-  
-//             </div>
-//         )
-//     }
-
-// }
-
-// export default Canvas;
+import './canvas.css';
 
 
-var OrbitControls = require("three-orbit-controls")(THREE);
+
 class Canvas extends React.Component {
     constructor(props) {
         super(props);
-        this.animate = this.animate.bind(this);
-        this.addCube = this.addCube.bind(this);
-        this.initializeCamera = this.initializeCamera.bind(this);
-        this.initializeOrbits = this.initializeOrbits.bind(this);
+
+        this.lastX = 0;
+        this.lastY = 0;
+
+        // this.canvas = document.getElementById('canvas');
+        // this.ctx = this.canvas.getContext('2d');
+        this.drawArea = React.createRef;
+        this.state ={
+            isDrawing: false,
+            // lines: Immutable.List(),
+        };
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.draw = this.draw.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
     }
 
-    addCube(cube) {
-        this.scene.add(cube);
+ 
+    componentDidMount(){
+        document.addEventListener('mousedown', this.handleMouseDown);
+        document.addEventListener('mousemove', this.draw);
     }
-    componentDidMount() {
-        const width = this.mount.clientWidth;
-        const height = this.mount.clientHeight;
-        this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.renderer.setSize(width, height);
-        this.mount.appendChild(this.renderer.domElement);
-        this.initializeOrbits();
-        this.initializeCamera();
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0xFF00FF });
-        let cube = new THREE.Mesh(geometry, material);
-        this.scene.add(cube);
-        this.animate();
+    
+    componentDidUpdate(){
+        document.addEventListener('mouseup', this.handleMouseUp);
     }
-    componentWillUnmount() {
-        cancelAnimationFrame(this.frameId);
-        this.mount.removeChild(this.renderer.domElement);
+    
+    handleMouseUp() {
+        this.setState({isDrawing: false});
     }
-    initializeOrbits() {
-        this.controls.rotateSpeed = 1.0;
-        this.controls.zoomSpeed = 1.2;
-        this.controls.panSpeed = 0.8;
-    }
-    initializeCamera() {
-        this.camera.position.x = 0;
-        this.camera.position.y = 0;
-        this.camera.position.z = 4;
-    }
-    animate(cube) {
-        this.frameId = window.requestAnimationFrame(this.animate);
 
-        this.renderer.render(this.scene, this.camera);
-        // cube.rotation.x += 0.01;
-        // cube.rotation.y += 0.01;
+    handleMouseDown(e) {
+        [this.lastX, this.lastY] = [e.offsetX, e.offsetY]
+        this.setState({ isDrawing: !this.state.isDrawing });
     }
+
+    draw(e){
+        if (this.state.isDrawing) {
+        let canvas = document.getElementById('canvas');
+        let ctx = canvas.getContext('2d');
+        ctx.strokeStyle = '#BADA55';
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.lineWidth = 10;
+        ctx.beginPath();
+        ctx.moveTo(this.lastX, this.lastY);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        [this.lastX, this.lastY] = [e.offsetX, e.offsetY];
+        }
+    }
+
+
+
+
 
     render() {
         return (
-            <div>
-                <div
-                    id="boardCanvas"
-                    style={{ width: "80vw", height: "40vw" }}
-                    ref={mount => {
-                        this.mount = mount;
-                    }}
-                />
+            <div >
+                <canvas ref={this.drawArea} id='canvas' />
             </div>
         );
     }
--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -6,10 +6,8 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>A Twitter Clone</h1>
                 <Canvas />
                 <footer>
-                    Copyright &copy; 2019 Chirper
                 </footer>
             </div>
         );
