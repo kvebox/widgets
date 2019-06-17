de335b1f936bf5de31af648475506c7830c6a048
victoria <vjohkv@gmail.com>
2019-06-17 10:08:54 -0700

    Allow uploading of custom text files

--- frontend/src/components/game/word.js
+++ frontend/src/components/game/word.js
@@ -9,27 +9,43 @@ class Word extends React.Component {
         this.handleFiles = this.handleFiles.bind(this);
     }
 
-    handleFiles(e){
-            var input = e.target;
 
-            var reader = new FileReader();
-            reader.onload = function() {
-                var text = reader.result;
-                var node = document.getElementById('text');
-                node.innerText = text;
-                console.log(reader.result.substring(0,5) );
-            };
-            // reader.readAsText(input.files[0]);
-
+    chooseWord(){
+        
     }
+
+    handleFiles(e){
+        let fileInput = document.getElementById('fileInput');
+        // console.log(e.target[0]);
+        var file = fileInput.files[0];
+        var textType= /text.*/;
+        console.log(file);
+
+
+        // if (file.type.match(textType)){
+        //     var reader = new FileReader();
+
+        //     reader.onload = function(e) {
+        //         var content = reader.result;
+        //         let textByLine = content.split('\n');
+        //         alert(textByLine);
+        //     }
+        //     reader.readAsText(file);
+        // } else {
+        //     alert('File not supported');
+        // }
+    }
+
+
 
 
     render() {
         return (
             <div>
-                {this.handleFiles(defaultFile)}
+
                 <div id ='text'></div>
-                <input type='file' accept='text/plain' onChange={(e) => this.handleFiles(e)} />
+                <input id='fileInput' type='file' onChange={e => this.handleFiles(e)}/>
+
                 <div id= 'output'></div>
 
             </div>
