65317680b8b549fcac6fa93a84c9d2621368b29c
victoria <vjohkv@gmail.com>
2019-06-17 16:25:43 -0700

    Apply placeholder gameplay

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Canvas from '../canvas/canvas';
-import Word from '../game/word';
+import Game from '../game/game';
 import './main.css';
 
 class MainPage extends React.Component {
@@ -8,7 +8,7 @@ class MainPage extends React.Component {
     render() {
         return (
             <div id='mainContainer'>
-                <Word />
+                <Game />
                 <Canvas />
             </div>
         );
