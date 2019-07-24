a578d581545bdde9dfd4b16f6bf60d0ccea70cb2
victoria <vjohkv@gmail.com>
2019-07-24 16:31:17 -0700

    Create + add logo

--- frontend/src/components/nav/headerNav.js
+++ frontend/src/components/nav/headerNav.js
@@ -7,6 +7,7 @@ class HeaderNav extends React.Component {
     render() {
         return (
             <div id='navigationContainer'>
+                <img id='logo' src='/ui/pixicon_logo.png'/>
             </div>
         )
     }
