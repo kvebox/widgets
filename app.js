a1c4d22be862340db7182f57d5dd797d7fdb6d5c
victoria <vjohkv@gmail.com>
2019-07-25 19:25:08 -0700

    Prepare for heroku push

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const passport = require('passport');
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+// Heroku Deployment 
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
+
 //importing routes
 const users = require("./routes/api/users");
 
